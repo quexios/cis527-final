9ee14662eee1706fe5903187e083c6f4c62c8ba5
quexios <quexios@gmail.com>
2025-12-19 16:46:29 -0600

    fix: update for puppet

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,15 +1,37 @@
+
 node default {
-  package { ['docker.io', 'docker-compose', 'ufw']:
+
+  package { ['docker.io', 'docker-compose-v2', 'ufw']:
     ensure => installed,
   }
 
   service { 'docker':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => Package['docker.io'],
   }
 
-  exec { 'ufw-allow-http':
+  exec { 'allow-ssh':
+    command => '/usr/sbin/ufw allow 22/tcp',
+    unless  => '/usr/sbin/ufw status | grep "22/tcp.*ALLOW"',
+    require => Package['ufw'],
+  }
+
+  exec { 'allow-http':
     command => '/usr/sbin/ufw allow 80/tcp',
     unless  => '/usr/sbin/ufw status | grep "80/tcp.*ALLOW"',
+    require => Package['ufw'],
+  }
+
+  exec { 'allow-backend':
+    command => '/usr/sbin/ufw allow 3000/tcp',
+    unless  => '/usr/sbin/ufw status | grep "3000/tcp.*ALLOW"',
+    require => Package['ufw'],
+  }
+
+  exec { 'enable-ufw':
+    command => '/usr/sbin/ufw --force enable',
+    unless  => '/usr/sbin/ufw status | grep "Status: active"',
+    require => [Exec['allow-ssh'], Exec['allow-http']],
   }
 }
